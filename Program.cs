82ddbdd643602dc9588fa443cf2867a0c330d7ab
Alexander <mypost@example.com>
2022-12-18 14:27:42 +0300

    13

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//task 13
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 10;
+
+if ((num / 100) % count == 0){
+    Console.WriteLine($"Третьей цифры в числе {num} нет");
+}
+else{
+    while ((num / count) % 10 != 0)
+    {
+        count = count * 10;
+        }
+    Console.WriteLine((num / (count/1000)) % 10);
+    // Console.WriteLine(count);
+}
